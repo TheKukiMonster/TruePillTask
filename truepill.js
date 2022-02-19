aa8d199598b69575c51e9ae881792ab1943cf98c
TheKukiMonster <ask1903@btinternet.com>
2022-02-19 00:44:08 +0000

    Basic formulary listing and JSON interaction in inquirer

--- truepill.js
+++ truepill.js
@@ -1,6 +1,7 @@
 'use strict';
 const inquirer = require('inquirer');
 
+const formularyJSON = require('./Data/formulary.json')
 
 const output = [];
 
@@ -23,8 +24,40 @@ const mainMenuQuestions = [
     }
 ]
 
+const typeMedicineName = [
+    {
+        type: 'input',
+        name: 'medNameInput',
+        message: 'Please enter the name of the medicine you wish to add to the formulary...'
+    }
+]
 
-function mainMenu(){
+
+//Check if the input medicine is valid, or already exists in the formulary
+async function checkString(string)
+{
+
+    var medicineList = getMedicineList();
+    console.log(medicineList)
+    var validMessage = "Valid Medicine";
+
+    if(string.length == 0)
+    {
+        return "Error. Please enter a medicine name"; 
+    }
+
+    medicineList.forEach(function(medicine) {
+        if(medicine === string)
+        {
+            validMessage = "Error. Medicine already exists in formulary";
+        }
+    })
+
+    return validMessage;
+
+}
+
+async function mainMenu(){
     inquirer.prompt(mainMenuQuestions).then((answers) => {
      //   output.push(answers.mainMenu);
 
@@ -34,40 +67,84 @@ function mainMenu(){
         switch(answers.mainMenu) {
 
             case 'Check the formulary':
-                console.log("Picked check formulary");
-                fetchJSONfile(0, 0);
+                //console.log("Picked check formulary");
+                fetchJSONfile(0);
+                break;
 
             case 'Check the stock tables':
-                console.log("Picked check stock tables");            
+                console.log("Picked check stock tables"); 
+                break;          
 
             case 'Add medicine to formulary':
                 console.log("Picked add medicine to formulary");
+                var medicineName
+                inquirer.prompt(typeMedicineName).then((answers => {   
+
+                    console.log(answers.medNameInput);
+                    var isValidName = Promise.resolve(checkString(answers.medNameInput));
+
+                    isValidName.then(function(message) {
+                        console.log(message);
+                    })
+
+                }));
+                break;
 
             case 'Add medicine to formulary':
                 console.log("Picked add medicine to stock tables");
+                break;
         }
 
     }
     )};
 
+async function getMedicineName(){
+
+
+    
+}
+
+//Adds to the relevant json file
+async function addToJSONfile(adding, medName)
+{
+
+}
+
+function getMedicineList()
+{
+    try {
+        var medicineList = formularyJSON['medicines']
+
+    } catch (error) {
+        console.log("Error reading the formulary database.");
+        return;
+    }
+
+    for(var str in medicineList)
+    {
+        console.log("medicine:" + str)
+    }
+    return medicineList;
+
+}
+
 //Fetches the relevant json file
 //adding is a boolean to decide which file to fetch. 
 //0 is formulary
 //1 is stock table
-function fetchJSONfile(adding, stock)
+async function fetchJSONfile(stock)
 {
     //fetch the formulary
     if(!stock){
 
-        //Only check stock
-        if(!adding)
-        {
-            
-        }
+        var medicineList = getMedicineList();
+        console.log(medicineList)
+        medicineList.forEach(medicine => console.log(medicine));
+        return;
     }
 }
 
-function parseJSON(json){
+async function parseJSON(json){
 
     keyList = json.keys;
 
